fe37de8abd1715f9bdf40c5e0abf767020be29f2
vasunuformsocial <vasu@nuformsocial.com>
2023-08-16 17:39:54 +0530

    adding modal

---
+++ script.js
@@ -0,0 +1,13 @@
+// let modal = document.querySelector(".modal");
+
+
+// function openModal() {
+//     console.log("modal is open");
+//     modal.style.scale = 1;
+// } 
+
+
+function toggleModal() {
+     document.getElementById('#modal').classList.toggle('hidden');
+     console.log("running");
+}
